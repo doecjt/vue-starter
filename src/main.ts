ff4503b185b5b70212c07ba7c1b5107299cf5f22
doecjt <doecjt@gmail.com>
2023-12-05 13:56:19 +0800

    feat: use tailwind-compat.css to reset style

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '@/router'
 
+import '@unocss/reset/tailwind-compat.css'
 import 'virtual:uno.css'
 
 const pinia = createPinia()
